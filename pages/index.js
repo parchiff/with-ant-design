2933fca0f2c759b524cfc2115057239bf02a57ba
Lukasz Kurowski <crackcomm@gmail.com>
2017-11-07 14:44:05 +0100

    feat(sider): shadow

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import BaseLayout from '../components/layout/BaseLayout.js';
 import { stylesheet, classNames } from './styles.css'
 
 const { Header, Sider, Content } = Layout;
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 export default class SiderDemo extends React.Component {
   state = {
@@ -24,21 +26,34 @@ export default class SiderDemo extends React.Component {
             collapsible
             collapsed={this.state.collapsed}
             collapsedWidth={0}
+            className={classNames.sider}
           >
             <div className={classNames.logo}></div>
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1">
-                <Icon type="user" />
-                <span>nav 1</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="video-camera" />
-                <span>nav 2</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Icon type="upload" />
-                <span>nav 3</span>
-              </Menu.Item>
+              <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                <MenuItemGroup key="g1" title="Item 1">
+                  <Menu.Item key="1">Option 1</Menu.Item>
+                  <Menu.Item key="2">Option 2</Menu.Item>
+                </MenuItemGroup>
+                <MenuItemGroup key="g2" title="Item 2">
+                  <Menu.Item key="3">Option 3</Menu.Item>
+                  <Menu.Item key="4">Option 4</Menu.Item>
+                </MenuItemGroup>
+              </SubMenu>
+              <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
+                <Menu.Item key="5">Option 5</Menu.Item>
+                <Menu.Item key="6">Option 6</Menu.Item>
+                <SubMenu key="sub3" title="Submenu">
+                  <Menu.Item key="7">Option 7</Menu.Item>
+                  <Menu.Item key="8">Option 8</Menu.Item>
+                </SubMenu>
+              </SubMenu>
+              <SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
+                <Menu.Item key="9">Option 9</Menu.Item>
+                <Menu.Item key="10">Option 10</Menu.Item>
+                <Menu.Item key="11">Option 11</Menu.Item>
+                <Menu.Item key="12">Option 12</Menu.Item>
+              </SubMenu>
             </Menu>
           </Sider>
           <Layout>
@@ -49,6 +64,11 @@ export default class SiderDemo extends React.Component {
                 onClick={this.toggle} />
             </Header>
             <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+              <h1>Hello world</h1>
+              Content
+              <h2>Hello world</h2>
+              Content
+              <h3>Hello world</h3>
               Content
             </Content>
           </Layout>
